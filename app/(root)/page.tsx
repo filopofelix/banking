b02098e4bc80dd4ab794d7edf88606c7d04b2305
filopofelix <felipfuentealbaci@gmail.com>
2024-07-23 01:01:42 -0400

    Banking App 3730 video agregar p

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
+import HeaderBox from '@/components/ui/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = { firstName: 'Felipe'};
+
   return (
-    <div>Home</div>
+    <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox 
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || 'Guest'}
+                    subtext="Acces and manage youe account and transaction efficiently"
+                />
+            </header>
+        </div>    
+    </section>
   )
 }
 
