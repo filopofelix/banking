9b9f9c9084c5defba0e30be09cfccb588e472f7e
filopofelix <felipfuentealbaci@gmail.com>
2024-07-25 01:12:09 -0400

    error en imagenes sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
-import HeaderBox from '@/components/ui/HeaderBox'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
@@ -12,8 +13,15 @@ const Home = () => {
                     type="greeting"
                     title="Welcome"
                     user={loggedIn?.firstName || 'Guest'}
-                    subtext="Acces and manage youe account and transaction efficiently"
+                    subtext="Access and manage youe account and transaction efficiently"
                 />
+
+                <TotalBalanceBox
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1250.25}
+                />
+
             </header>
         </div>    
     </section>
