df3cab3682c04e1f9bd579f42aafb7f8f4711697
filopofelix <felipfuentealbaci@gmail.com>
2024-08-13 03:07:51 -0400

    implementando form regristro 2 16 43

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -2,7 +2,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import React, { useState } from 'react'
-
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -17,29 +16,29 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Divide } from 'lucide-react';
-
-const formSchema = z.object({
-    email: z.string().email(),
-  })
-   
-
+import CustomInput from './CustomInput';
+import { authFormSchema } from '@/lib/utils';
+import { Loader2 } from 'lucide-react';
 
 const AuthForm = ({ type }: { type: string }) => {
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(false)
     // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof authFormSchema>>({
+    resolver: zodResolver(authFormSchema),
     defaultValues: {
       email: "",
+      password: ""
     },
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof authFormSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
+    setIsLoading(true)
     console.log(values)
+    setIsLoading(false)
   }
     return (
         <section className='auth-form'>
@@ -66,7 +65,7 @@ const AuthForm = ({ type }: { type: string }) => {
                      <p className='text-16 font-normal text-gray-600'>
                         {user
                             ? 'Registra tu cuenta para comenzar'
-                            : 'Porfavor ingresa tus datos'
+                            : 'Por favor ingresa tus datos'
                         }
                      </p>
                     </h1>
@@ -77,48 +76,29 @@ const AuthForm = ({ type }: { type: string }) => {
                     {/* PlaidLink */}
                 </div>
             ): (
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                        <FormField
-                            control={form.control}
-                            name="email"
-                            render={({ field }) => (
-                                <div className='form-item'>
-                                    <FormLabel className='form-label'>
-                                        Email
-                                    </FormLabel>
-                                    <div className='flex w-full flex-col'>
-                                        <FormControl>
-                                            <Input placeholder='Ingresa tu correo' className='import-class' {...field}>
-                                            </Input>
-                                        </FormControl>
-                                        <FormMessage className='form-message mt-2'/>
-                                    </div>
-                                </div>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="password"
-                            render={({ field }) => (
-                                <div className='form-item'>
-                                    <FormLabel className='form-label'>
-                                        Contraseña
-                                    </FormLabel>
-                                    <div className='flex w-full flex-col'>
-                                        <FormControl>
-                                            <Input placeholder='Ingresa tu contraseña' className='import-class' {...field}>
-                                            </Input>
-                                        </FormControl>
-                                        <FormMessage className='form-message mt-2'/>
-                                    </div>
-                                </div>
-                            )}
-                        />
-                        <Button type="submit">Submit</Button>
-                    </form>
-                
-                </Form>
+                <>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            
+                            <CustomInput control={form.control} name='email' label='Email' placeholder='Ingresa tu Usuario'/>
+                            <CustomInput control={form.control} name='password' label='Contraseña' placeholder='Ingresa tu Contraseña'/>
+                            <div className='flex flex-col gap-4'>
+                                <Button type="submit" disabled={isLoading} className='form-btn'>{isLoading ? (
+                                    <>
+                                        <Loader2 size={20} className='animate-spin'/> &nbsp;
+                                        Cargando...
+                                    </>
+                                ) : type === 'sign-in' ? 'Ingresar' : 'Registrar'}
+                                </Button>
+                            </div>
+                           
+                        </form>
+                    </Form>
+                    <footer className='flex justify-center gap-1'>
+                        <p className='text-14 font-normal text-gray-600'>{type === 'sign-in' ? 'Aún no tengo cuenta?' : 'Ya tengo mi cuenta!'}</p>
+                        <Link href={type === 'sign-in' ? '/sign-up' : 'sign-in'} className='form-link'>{type === 'sign-in' ? 'Registrarme' : 'Ingresar'}</Link>
+                    </footer>
+                </>
             )}
         </section>
     )
